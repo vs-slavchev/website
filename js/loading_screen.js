780aabdd60bac2b9dd34255279c38d34e616061f
vs-slavchev <vs_slavchev2@abv.bg>
2019-09-24 21:47:22 +0200

    Add article date and tags

--- js/loading_screen.js
+++ js/loading_screen.js
@@ -1,3 +1,4 @@
+// uncomment to finish loading animation when all images are loaded
 /*window.addEventListener("load", function(){
     finish_loading_animation();
 });*/
@@ -12,53 +13,57 @@ function sleep(ms) {
 }
 
 function add_line_to_loading_screen(line) {
-    var para = document.createElement("P");
-    var t = document.createTextNode(line);
-    para.appendChild(t);
-    document.getElementById("loading-screen").appendChild(para);
+    var p_element = document.createElement("P");
+    var text = document.createTextNode(line);
+    p_element.appendChild(text);
+    document.getElementById("loading-screen").appendChild(p_element);
 }
 
-function get_random_time_step() {
-    return 10 + Math.floor(Math.random() * 121);
+function get_time_step(wait_factor) {
+    wait_factor = wait_factor || Math.floor(Math.random() * 5);
+    return 30 + 90 * wait_factor;
 }
 
 async function run_loading_screen() {
     await sleep(100);
     add_line_to_loading_screen("Starting setup virtual console...");
-    await sleep(get_random_time_step());
+    await sleep(get_time_step(4));
     add_line_to_loading_screen("Loading initial ramdisk...");
-    await sleep(get_random_time_step());
+    await sleep(get_time_step(4));
     add_line_to_loading_screen("Creating system user injector...");
-    await sleep(get_random_time_step());
+    await sleep(get_time_step());
     add_line_to_loading_screen("Compiling kernel device manager...");
-    await sleep(get_random_time_step());
+    await sleep(get_time_step());
     add_line_to_loading_screen("Rebuilding dynamic linker cache...");
-    await sleep(get_random_time_step());
+    await sleep(get_time_step());
     add_line_to_loading_screen("Registering backchannel transport module...");
-    await sleep(get_random_time_step());
-    add_line_to_loading_screen("Trigger flushing of persistent storage...");
-    await sleep(get_random_time_step());
+    await sleep(get_time_step());
+    add_line_to_loading_screen("Triggering persistent storage flush...");
+    await sleep(get_time_step());
     add_line_to_loading_screen("Reaching target port mapper...");
-    await sleep(get_random_time_step());
+    await sleep(get_time_step());
     add_line_to_loading_screen("Checking partition sensor resolution...");
-    await sleep(get_random_time_step());
+    await sleep(get_time_step());
     add_line_to_loading_screen("Mounting debug file system...");
-    await sleep(get_random_time_step());
+    await sleep(get_time_step());
     add_line_to_loading_screen("Monitoring temporary queue takeover...");
-    await sleep(get_random_time_step());
+    await sleep(get_time_step());
     add_line_to_loading_screen("Removing root-only control...");
-    await sleep(get_random_time_step());
+    await sleep(get_time_step());
     add_line_to_loading_screen("Building supplier repository adapter...");
-    await sleep(get_random_time_step());
-    add_line_to_loading_screen("Issuing functional concurrency constructor...");
-    await sleep(get_random_time_step());
+    await sleep(get_time_step());
+    add_line_to_loading_screen("Issuing functional concurrency destructor...");
+    await sleep(get_time_step());
     add_line_to_loading_screen("Reverting watchdog polling routine...");
-    await sleep(get_random_time_step());
+    await sleep(get_time_step(4));
     add_line_to_loading_screen("Importiing network configuration loader...");
-    await sleep(get_random_time_step());
+    await sleep(get_time_step(4));
     add_line_to_loading_screen("Binding lock filter daemon...");
-    await sleep(150);
+    await sleep(get_time_step());
+    add_line_to_loading_screen("Done... Starting...");
+    await sleep(350);
     finish_loading_animation();
 }
 
 run_loading_screen();
+//finish_loading_animation();
